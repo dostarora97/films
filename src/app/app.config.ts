ff492cea4fc911a82aedfa3d4dd96ea9f25e9dcc
I533118 <dost.arora@sap.com>
2024-03-29 09:37:16 +0530

    Reduce size

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,5 +5,5 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync('noop')]
+  providers: [provideAnimationsAsync('noop')]
 };
